410942c5f4b58cef077f2d6d7ae455a8830323e9
Aaron Mell <aaronamell@gmail.com>
2010-12-19 10:04:06 -0600

    Initial Commit of files

---
+++ MapTiler/Tiler.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MapTiler
+{
+    partial class Tiler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tiler));
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.latitude = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.longitude = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MainMap
+            // 
+            this.MainMap.Bearing = 0F;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.LevelsKeepInMemmory = 5;
+            this.MainMap.Location = new System.Drawing.Point(24, 13);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 17;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.Position = ((GMap.NET.PointLatLng)(resources.GetObject("MainMap.Position")));
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(730, 404);
+            this.MainMap.TabIndex = 0;
+            this.MainMap.Zoom = 0D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Stitch Map";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coordinates";
+            // 
+            // latitude
+            // 
+            this.latitude.Location = new System.Drawing.Point(58, 440);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(71, 20);
+            this.latitude.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lat:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 466);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lon:";
+            // 
+            // longitude
+            // 
+            this.longitude.Location = new System.Drawing.Point(58, 466);
+            this.longitude.Name = "longitude";
+            this.longitude.Size = new System.Drawing.Size(71, 20);
+            this.longitude.TabIndex = 5;
+            // 
+            // Tiler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.longitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.latitude);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainMap);
+            this.Name = "Tiler";
+            this.Text = "Map Tiler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox latitude;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox longitude;
+    }
+}
+
---
+++ MapTiler/Tiler.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using GMap.NET;
+using System.Globalization;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace MapTiler
+{
+    public partial class Tiler : Form
+    {
+        private GMapMarker center;
+        private GMapOverlay top;
+        private bool isMouseDown;
+        private PointLatLng startPosition;
+        private PointLatLng endPosition;
+
+        public Tiler()
+        {
+            InitializeComponent();
+
+            if (!DesignMode)
+            {
+                // add your custom map db provider
+                //GMap.NET.CacheProviders.MySQLPureImageCache ch = new GMap.NET.CacheProviders.MySQLPureImageCache();
+                //ch.ConnectionString = @"server=sql2008;User Id=trolis;Persist Security Info=True;database=gmapnetcache;password=trolis;";
+                //MainMap.Manager.ImageCacheSecond = ch;
+
+                // set your proxy here if need
+                //MainMap.Manager.Proxy = new WebProxy("10.2.0.100", 8080);
+                //MainMap.Manager.Proxy.Credentials = new NetworkCredential("ogrenci@bilgeadam.com", "bilgeada");
+
+                // set cache mode only if no internet avaible
+                try
+                {
+                    System.Net.IPHostEntry e = System.Net.Dns.GetHostEntry("www.google.com");
+                }
+                catch
+                {
+                    MainMap.Manager.Mode = AccessMode.CacheOnly;
+                    MessageBox.Show("No internet connection avaible, going to CacheOnly mode.", "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+                // config map 
+                MainMap.Position = new PointLatLng(35.2276723549358, -97.22351074);
+
+                MainMap.MapType = MapType.GoogleMap;
+                MainMap.MinZoom = 1;
+                MainMap.MaxZoom = 17;
+                MainMap.Zoom = 10;
+
+                
+                latitude.Text = MainMap.Position.Lat.ToString(CultureInfo.InvariantCulture);
+                longitude.Text = MainMap.Position.Lng.ToString(CultureInfo.InvariantCulture);
+
+                top = new GMapOverlay(MainMap, "top");
+                MainMap.Overlays.Add(top);
+
+
+                // map center
+                center = new GMapMarkerCross(MainMap.Position);
+                top.Markers.Add(center);
+
+                // map events
+                MainMap.OnCurrentPositionChanged += new CurrentPositionChanged(MainMap_OnCurrentPositionChanged);               
+                MainMap.MouseMove += new MouseEventHandler(MainMap_MouseMove);
+                MainMap.MouseDown += new MouseEventHandler(MainMap_MouseDown);
+                MainMap.MouseUp += new MouseEventHandler(MainMap_MouseUp);
+
+                MainMap.MapType = MapType.GoogleMap;
+
+                MainMap.ShowTileGridLines = true;
+            }
+        }
+
+        #region Events       
+        
+        // current point changed
+        void MainMap_OnCurrentPositionChanged(PointLatLng point)
+        {
+            center.Position = point;
+            latitude.Text = point.Lat.ToString(CultureInfo.InvariantCulture);
+            longitude.Text = point.Lng.ToString(CultureInfo.InvariantCulture);
+        }        
+
+        // ensure focus on map, trackbar can have it too
+        private void MainMap_MouseEnter(object sender, EventArgs e)
+        {
+            MainMap.Focus();
+        }
+
+        void MainMap_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+                endPosition = MainMap.FromLocalToLatLng(e.X, e.Y);
+            }
+        }
+
+        void MainMap_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = true;
+
+                startPosition = MainMap.FromLocalToLatLng(e.X, e.Y);
+
+                //if (currentMarker.IsVisible)
+                //{
+                //    currentMarker.Position = MainMap.FromLocalToLatLng(e.X, e.Y);
+
+                //    var px = MainMap.Projection.FromLatLngToPixel(currentMarker.Position.Lat, currentMarker.Position.Lng, (int)MainMap.Zoom);
+                //    var tile = MainMap.Projection.FromPixelToTileXY(px);
+
+                //    Debug.WriteLine("marker: " + currentMarker.LocalPosition + " | geo: " + currentMarker.Position + " | px: " + px + " | tile: " + tile);
+                //}
+            }
+        }
+
+        // move current marker with left holding
+        void MainMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && isMouseDown)
+            {
+                //if (CurentRectMarker == null)
+                //{
+                //    if (currentMarker.IsVisible)
+                //    {
+                //        currentMarker.Position = MainMap.FromLocalToLatLng(e.X, e.Y);
+                //    }
+                //}
+                //else // move rect marker
+                //{
+                //    PointLatLng pnew = MainMap.FromLocalToLatLng(e.X, e.Y);
+
+                //    int? pIndex = (int?)CurentRectMarker.Tag;
+                //    if (pIndex.HasValue)
+                //    {
+                //        if (pIndex < polygon.Points.Count)
+                //        {
+                //            polygon.Points[pIndex.Value] = pnew;
+                //            MainMap.UpdatePolygonLocalPosition(polygon);
+                //        }
+                //    }
+
+                //    if (currentMarker.IsVisible)
+                //    {
+                //        currentMarker.Position = pnew;
+                //    }
+                //    CurentRectMarker.Position = pnew;
+
+                //    if (CurentRectMarker.InnerMarker != null)
+                //    {
+                //        CurentRectMarker.InnerMarker.Position = pnew;
+                //    }
+                //}
+            }
+        }
+
+        #endregion     
+    }
+}

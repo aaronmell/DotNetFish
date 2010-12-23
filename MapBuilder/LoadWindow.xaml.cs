48f75f43a9a05311a26cbd1842dbdedf37f3fd48
Aaron Mell <aaronamell@gmail.com>
2010-12-22 21:26:27 -0600

    Added the MapBuilder Project to the Solution

---
+++ MapBuilder/LoadWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MapBuilder
+{
+    /// <summary>
+    /// Interaction logic for LoadWindow.xaml
+    /// </summary>
+    public partial class LoadWindow : Window
+    {
+        public LoadWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxNewLevel_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //dlg.FileName = "DotNetFish Level"; // Default file name
+            dlg.DefaultExt = ".dfm"; // Default file extension
+            dlg.Filter = "DotNetFish Map (.dfm)|*.dfm"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+        }       
+
+        private void uxLoadLevel_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //dlg.FileName = "DotNetFish Level"; // Default file name
+            dlg.DefaultExt = ".dfl"; // Default file extension
+            dlg.Filter = "DotNetFish Level (.dfl)|*.dfl"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+            }
+
+        }
+    }
+}
